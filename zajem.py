9b8e3eaa5e3f65469c49e66ccd8ca6ea63a1bb85
Matej Šafarič <matej.safaric@gmail.com>
2022-11-07 21:29:19 +0100

    Zacel s parsanjem

--- zajem.py
+++ zajem.py
@@ -1,10 +1,31 @@
 import orodja
 import re
+# temp
+import requests as req
 
-vzorec_bloka = re.compile(
-    r'<a href="https:\/\/store\.steampowered\.com\/app\/.*?'
-    r'<div style="clear: left;"><\/div>',
+url_sample = re.compile(
+    r'<a href="(https:\/\/store\.steampowered\.com\/app\/.*?\/)\?snr=1_7_7',
     flags=re.DOTALL
     )
 
-orodja.shrani_spletno_stran('https://store.steampowered.com/search/?category1=998&filter=topsellers', 'html.txt')
+game_sample = re.compile(
+    r'',
+    flags=re.DOTALL
+)
+
+with open('html.txt', encoding='UTF-8') as d:
+    data = d.read()
+
+# Temp sample code
+with open('sample_html.txt', encoding='UTF-8') as d:
+    sample = d.read()
+
+def gamesite_parse(html_code):
+    pass
+
+for url in url_sample.finditer(sample):
+    r = req.get(url.group(1))
+    t = r.text
+    gamesite_parse()
+
+
